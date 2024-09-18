cbbdc23b1894f8dbbdd1e62fbe4032f2f42b09a0
Roman Shantsila <roman.shantsila@warwick.ac.uk>
2024-09-18 23:30:14 +0100

    Update mace_nvt.py
    added comments

--- mace_nvt.py
+++ mace_nvt.py
@@ -13,27 +13,32 @@ from ase.md.analysis import DiffusionCoefficient
 
 
 
-
+## read in the command line inputs from the bash run script
 init_file,temp,run,ts,tts,step,model,floats,device = sys.argv[1:]
+
+## set the random seed unique for each repeat
 random.seed(run)
+
 print('initialise')            
 ## read in init structure
 init_conf = read(init_file)
             
-## correct rounding error
+## When reading the VASP POSCAR file, later saved as .xyz, 0.0 can be read as an infenitesimal number correct this to be 0 as NVT dynamics requires upper triangle
 a = init_conf.get_cell()
 a[1,0]=0.0;a[2,0]=0.0;a[2,1]=0.0
 init_conf.set_cell(a)
             
-## launch and set mace calculator
+## launch and set mace calculator, if small, medium or large is specified then the use of mace mp is implied, else a path to the mace model is read in a used
 mp_models = ['small','medium','large']
 if model.strip().lower() in mp_models:
     calc = mace_mp(model=model, device=device, default_dtype=floats)
 else:
     calc = MACECalculator(model_paths=[model], device=device, default_dtype=floats)
 init_conf.calc = calc
-            
+
+## remove previous repeats, this avoids any append to structures, this can be suppressed but for the purpose of this result it could make the process more confusing
 os.system('rm mace_md_'+str(temp)+'K_repeat_'+str(run)+'.xyz')
+
 ## define write file format 
 def write_frame():
     dyn.atoms.write('mace_md_'+str(temp)+'K_repeat_'+str(run)+'.xyz', append=True)
